0f7b90f0971ad1736f85a172afa45a0948af9801
Steve Barnett <mooped@gmail.com>
2026-03-15 20:56:04 +0000

    Decoded handles raw bytes (accounting for a guess at whatever unicode shenanigans Mosquitto is doing).

--- decode.py
+++ decode.py
@@ -1,23 +1,28 @@
 import base64
+import binascii
 import sys
 
 #print(base64.b64encode(''.join(map(chr, range(84))).encode()))
 
 def output(buffer):
-    image = base64.b64decode(buffer)
-    image = list(map(lambda x: [c for c in "{0:8b}".format(x)], image))
-    image = zip(*image)
+  image = None
+  try:
+    image = base64.b64decode(buffer, validate=True)
+  except binascii.Error:
+    image = list(map(lambda x: ord(x), buffer.decode(encoding='utf-8')))
+  image = list(map(lambda x: [c for c in "{0:8b}".format(x)], image))
+  image = zip(*image)
 
-    print("+- FLIPDOT SIMULATOR ----------------------------------------------------------------+")
+  print("+- FLIPDOT SIMULATOR ----------------------------------------------------------------+")
 
-    for row in image:
-        buffer = ["+"]
-        buffer.extend(''.join(row).replace('0', ' ').replace('1', '#'))
-        buffer.append("+")
-        print(''.join(buffer))
+  for row in image:
+      buffer = ["+"]
+      buffer.extend(''.join(row).replace('0', ' ').replace('1', '#'))
+      buffer.append("+")
+      print(''.join(buffer))
 
-    print("+------------------------------------------------------------------------ MOOP 2026 -+")
+  print("+------------------------------------------------------------------------ MOOP 2026 -+")
 
 if __name__ == "__main__":
-    output(sys.argv[1])
+  output(sys.argv[1])
 
