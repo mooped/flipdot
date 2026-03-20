280fa899e80ebd641cfbe309094cc2e99bda373c
Steve Barnett <mooped@gmail.com>
2026-03-20 17:11:59 +0000

    Orrerey thing

--- vexate.py
+++ vexate.py
@@ -32,8 +32,14 @@ def muls(a, b):
 def divs(a, b):
   return nary(lambda x: x / b, a)
 
+def dot(a, b):
+  return sum(mul(a, b))
+
+def lengthsq(a):
+  return dot(a, a)
+
 def length(a):
-  return math.sqrt(sum(mul(a, a)))
+  return math.sqrt(dot(a, a))
 
 def normalize(a):
   return divs(a, length(a))
@@ -77,6 +83,41 @@ def dist(x, y, z, t):
   d = orrerey(x, y, z, t, (0.0, 0.0, 2380000000.0))
   return d#0 if math.modf(d * 0.1)[0] <= 0.5 else 1
 
+# Distance along ray until intersection with a sphere
+def raysphere(ray, pos, radius):
+  # Project ray onto pos, assume ray is already normalised
+  proj = dot(ray, pos)
+  # Nearest point on the ray to the centre of the sphere
+  nearest = muls(ray, proj)
+  # Distance squared from ray to centre of sphere
+  distsq = lengthsq(sub(nearest, pos))
+  # Did we hit?
+  if distsq < (radius * radius):
+    # Intersection point forms a triangle with the projected point and the sphere centre
+    intersection = sub(nearest, muls(ray, math.sqrt(radius * radius + distsq)))
+    return math.sqrt(distsq), intersection
+  return None
+
+def orreray(ray, t, pos):
+  ang = t * 2.0 * math.pi
+  planets = [
+    (pos, 695700.0 / 40.0),
+    (add(pos, muls((math.cos(ang / 88.0), math.sin(ang / 88.0), 0.0), 57900000)), 2440.0),
+    (add(pos, muls((math.cos(ang / 225.0), math.sin(ang / 225.0), 0.0), 108200000)), 6052.0),
+    (add(pos, muls((math.cos(ang / 365.0), math.sin(ang / 365.0), 0.0), 149600000)), 6371.0),
+    (add(pos, muls((math.cos(ang / 687.0), math.sin(ang / 687.0), 0.0), 227900000)), 3390.0),
+    (add(pos, muls((math.cos(ang / 4333.0), math.sin(ang / 4333.0), 0.0), 778600000)), 69911.0),
+    (add(pos, muls((math.cos(ang / 10759.0), math.sin(ang / 10759.0), 0.0), 1433500000)), 58232.0),
+    (add(pos, muls((math.cos(ang / 30687.0), math.sin(ang / 30687.0), 0.0), 2872500000)), 25362.0),
+    (add(pos, muls((math.cos(ang / 60190.0), math.sin(ang / 60190.0), 0.0), 4495100000)), 24622.0),
+  ]
+  nearest = None
+  for (p, r) in planets:
+    hit = raysphere(ray, p, r * 2000.0)
+    if hit:
+      return 1
+  return 0
+
 # Precompute ray directions (because why not)
 # Put the centre of the screen 20 units from the camera down z
 rays = []
@@ -86,7 +127,7 @@ for y in range(8):
     row.append(normalize((x - 41.5, y - 3.5, 20.0)))
   rays.append(row)
 
-def raycast(x, y, t):
+def raycastdf(x, y, t):
   ray = rays[y][x]
                                 # Start tracing at the screen to ignore anything behind it
   c = add(muls(ray, 20.0), (0.0, 0.0, 5.0 * t))
@@ -97,6 +138,10 @@ def raycast(x, y, t):
     if d < 100000000.0:         # Hit (close enough)
       return 1
   return 0                      # Miss
+
+def raycast(x, y, t):
+  ray = rays[y][x]
+  return orreray(ray, t, (0.0, 0.0, 238000000.0))#2380000000.0))
 
 def encode(grid):
   buffer = []
@@ -134,19 +179,19 @@ def send_raw(message):
 def send_text(message):
   mqttc.publish('nh/flipdot/comfy/text', message)
 
-set_buffer(6)
+#set_buffer(6)
 
-for t in range(120):
-  f = frame(0.1 * t)
+for t in range(365):
+  f = frame(t)
   #send_raw(f)
   if args.verbose:
     decode.output(f)
-  time.sleep(0.5)
+  #time.sleep(0.5)
 
-send_text("CONTAINMENT!")
+#send_text("CONTAINMENT!")
 
-time.sleep(10)
-set_buffer(0)
+#time.sleep(10)
+#set_buffer(0)
 
 mqttc.loop_stop()
 
